47736d4c65a1c7708c52c7c29d1c9a0434d57466
diego-cavenati <diegocavenati@gmail.com>
2023-01-19 15:01:56 +0100

    add api endpoint

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -20,7 +20,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::orderByDesc('id')->get();
+        $projects = Project::orderByDesc('id')->get()->paginate(5);
         return view('admin.projects.index', compact('projects'));
     }
 
